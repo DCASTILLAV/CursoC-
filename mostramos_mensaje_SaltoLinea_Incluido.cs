4cd76f9693fbfc5807d639984986268160606f71
danny castilla <jdannycastilla@outlook.com>
2023-11-28 12:54:12 -0500

    Agregamos mensaje con salto de linea incluida

---
+++ mostramos_mensaje_SaltoLinea_Incluido.cs
@@ -0,0 +1,17 @@
+
+    using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+           // Console.WriteLine("Hello world!");
+            //declarando las variables numericas
+            int num1;
+            int num2;
+            int suma;
+            num1=5; num2=7; suma=num1+num2;
+            Console.WriteLine("las variables se declararon correctamente \n" + " la suma es:" + suma);
+        }
+    }
+}
+
