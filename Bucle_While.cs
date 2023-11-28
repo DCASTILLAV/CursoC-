f5f9f10b8a7596620d0a21f45cc5bde39b42d754
danny castilla <jdannycastilla@outlook.com>
2023-11-28 13:58:24 -0500

    Agregamos bucle while

---
+++ Bucle_While.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+           // Console.WriteLine("Hello world!");        
+           //declarando las variables
+            int contador =0;
+            while (contador<10)
+            {
+                 Console.WriteLine($"¡Hola mundo!, el contador es {counter}");
+                 counter++;
+            }
+        }
+    }
+}
