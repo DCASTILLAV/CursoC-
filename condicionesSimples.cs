cf9a6c7377e3a0b38b646469b801ff809f2b3f81
danny castilla <jdannycastilla@outlook.com>
2023-11-28 12:59:12 -0500

    agregamos condicionalSimple

---
+++ condicionesSimples.cs
@@ -0,0 +1,16 @@
+using System;
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+           // Console.WriteLine("Hello world!");
+            //declarando las variables numericas
+      
+            int num1;
+            num1=5;
+            if (num1==5)
+            {
+            Console.WriteLine("El numero es 5");
+            }
+        }
+    }
+}
