31c0a50990694873756aa70eedba9973f37ad646
danny castilla <jdannycastilla@outlook.com>
2023-11-28 13:01:43 -0500

    agregamos condiciones dobles

---
+++ CondicionesDobles.cs
@@ -0,0 +1,20 @@
+using System;
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+           // Console.WriteLine("Hello world!");
+            //declarando las variables numericas
+      
+            int num1;
+            num1=5;
+            if (num1==5)
+            {
+            Console.WriteLine("El numero es 5");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es diferente a 5");
+            }
+        }
+    }
+}
